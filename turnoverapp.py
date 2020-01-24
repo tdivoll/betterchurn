6b95226d7685580ba64b7ac4c5488389c9e47eeb
tdivoll <timothy.divoll@gmail.com>
2020-01-24 16:05:52 -0500

    Update turnoverapp.py
    app title change

--- turnoverapp.py
+++ turnoverapp.py
@@ -32,7 +32,7 @@ def load_model(modelName):
 modelName = 'randomfor_week2'
 model = load_model(modelName)
 #
-st.title('Low-flow Nursing')
+st.title('BetterChuRN')
 st.header("Predicting RN turnover rates with staffing metrics, patient reviews, and employee sentiments")
 
 
@@ -41,7 +41,7 @@ st.header("Predicting RN turnover rates with staffing metrics, patient reviews, 
 #pred_data = pd.read_pickle(file, compression=None)
 
 
-data2 = pd.read_csv('/home/tjd/InsightFiles/turnover-app/betterchurn/TurnoverRates.csv')
+data2 = pd.read_csv('TurnoverRates.csv')
 columns = data2[['RN Turnover Rate in the Medical-Surgical Unit','RN Turnover Rate in the Critical Care Unit', 'RN Turnover Rate in the NICU', 'RN Turnover Rate in the Mother/Baby Unit']]
 
 
@@ -52,4 +52,4 @@ ax.set_xlim([0,100])
 st.bar_chart(data2[column], width = 7)
 
 
-st.dataframe(pd.read_csv('/home/tjd/InsightFiles/turnover-app/betterchurn/RelevantSurvey.csv'))
+st.dataframe(pd.read_csv('RelevantSurvey.csv'))
