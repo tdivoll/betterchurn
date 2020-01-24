68c601c1819e7631460dafdb00ee402ddd67607b
tdivoll <timothy.divoll@gmail.com>
2020-01-24 15:30:13 -0500

    Update turnoverapp.py

--- turnoverapp.py
+++ turnoverapp.py
@@ -1,18 +1,44 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.externals import joblib
+import os, sys
+import importlib.util
 import matplotlib.pyplot as plt
 import pickle
 
+if len(sys.argv) > 1:
+    folder = os.path.abspath(sys.argv[1])
+else:
+    folder = os.path.abspath(os.getcwd())
+    
+# Get filenames for all python files in this path, excluding this script
+thisFile = os.path.abspath(__file__)
+fileNames = []
+for baseName in os.listdir(folder):
+	fileName = os.path.join(folder, baseName)
+	if (fileName.endswith(".py")) and (fileName != thisFile):
+		fileNames.append(fileName)
+  
+# Filename formatter to display a nicer url (instead of the whole github path)
+def format_url(s):
+	els = s.split("/")[-1].split(".")[0].split("_")
+	return " ".join(el for el in els).capitalize()
+
+@st.cache
+def load_model(modelName):
+  model=pd.read_pickle(os.path.join(folder, 'models', modelName + '.sav'))
+  return model
+
+modelName = 'randomfor_week2'
+model = load_model(modelName
 
 st.title('Low-flow Nursing')
 st.header("Predicting RN turnover rates with staffing metrics, patient reviews, and employee sentiments")
 
 
 #load pickled model
-file = open('/home/tjd/InsightFiles/turnover-app/betterchurn/randomfor_week2.sav', 'rb')
-pred_data = pd.read_pickle(file, compression=None)
+#file = open('/home/tjd/InsightFiles/turnover-app/betterchurn/randomfor_week2.sav', 'rb')
+#pred_data = pd.read_pickle(file, compression=None)
 
 
 data2 = pd.read_csv('/home/tjd/InsightFiles/turnover-app/betterchurn/TurnoverRates.csv')
